175c47115c51f433b8e8376cbdc716362af498a6
Alexander Nakahara <alexander.nakahara@ludospro.com.br>
2020-04-05 17:54:37 -0300

    fix class

--- src/Servelets/DataAtual.java
+++ src/Servelets/DataAtual.java
@@ -12,8 +12,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.sun.tools.classfile.Opcode.Set;
-
 /**
  * Servlet implementation class DataAtual
  */
