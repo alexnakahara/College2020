67cef2b5bdc658681de77e66e55917b5a1553764
Alexander Nakahara <alexander.nakahara@ludospro.com.br>
2020-04-05 20:27:12 -0300

    Limpando o código

--- src/Servelets/Autenticacao.java
+++ src/Servelets/Autenticacao.java
@@ -25,25 +25,9 @@ public class Autenticacao extends HttpServlet {
 		// TODO Auto-generated constructor stub
 	}
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		// doGet(request, response);
-		RequestDispatcher rd = null;
+		
 		String nome = request.getParameter("name");
 		String senha = request.getParameter("password");
 		PrintWriter saida = response.getWriter();
