f4ce459932bf1a1bda37cc05eda8ca95dc6dc945
Alexander Nakahara <alexander.nakahara@ludospro.com.br>
2020-04-05 17:52:58 -0300

    Get data atual

---
+++ src/Servelets/DataAtual.java
@@ -0,0 +1,44 @@
+package Servelets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sun.tools.classfile.Opcode.Set;
+
+/**
+ * Servlet implementation class DataAtual
+ */
+@WebServlet("/DataAtual.do")
+public class DataAtual extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DataAtual() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		DateFormat df = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
+		Calendar calobj = Calendar.getInstance();
+		
+		response.setContentType("text/html");
+		PrintWriter saida  = response.getWriter();
+		saida.println("<h2>Data atual</h2><br/>" + df.format(calobj.getTime()));
+	}
+
+}
